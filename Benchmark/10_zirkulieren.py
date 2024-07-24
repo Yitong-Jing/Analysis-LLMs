e54001d69d16affa08ee5bd65bead436a79a2e9c
Yitong-Jing <63410816+Yitong-Jing@users.noreply.github.com>
2024-07-24 13:33:24 +0200

    Delete Benchmark/10_zirkulieren.py

--- Benchmark/10_zirkulieren.py
+++
@@ -1,5 +0,0 @@
-# LLM antwortet zehnmal in einer Schleife und führt jedes Mal ein Benchmarking durch
-import subprocess
-
-for i in range(10):
-    subprocess.call(["python", "rechnen_Benchmarks.py"])
